d6dc97cdd6ace03bbf8ce6d1606a70498bddd9a0
Charlotte Gore <conspiracygore@gmail.com>
2013-03-14 11:32:13 +0000

    Reversed auto-invoking of presets.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var Bezier = require('bezier');
 
 var presets = {
-	"ease-in" : Bezier({ c1 : [0,0], c2 : [0.43,0.01], c3 : [0.985,0.09], c4 : [1,1]}),
-	"ease-out" : Bezier({ c1 : [0,0], c2 : [0.015,0.83], c3 : [0.375,0.995], c4 : [1,1]}),
-	"ease-in-and-out" :	Bezier({ c1 : [0,0], c2 : [0.995,-0.015], c3 : [0.025,1.02], c4 : [1,1]}),
-	"linear" : Bezier({ c1 : [0,0], c2 : [1,1]}).isLinear(),
-	"inverse-in-and-out" : Bezier({ c1 : [0,0], c2 : [-0.045,0.72], c3 : [1.09,0.22], c4 : [1,1]}),
-	"unhook" : Bezier({ c1 : [0,0], c2 : [0.19,-0.72], c3 : [-0.05,1.085], c4 : [1,1]}),
-	"overshoot" : Bezier({ c1 : [0,0], c2 : [0.87,-0.095], c3 : [0.8,1.55], c4 : [1,1]}),
-	"pop" : Bezier({ c1 : [0,0], c2 : [0.595,-0.795], c3 : [0.46,1.6], c4 : [1,1]})
+	"ease-in" : { c1 : [0,0], c2 : [0.43,0.01], c3 : [0.985,0.09], c4 : [1,1]},
+	"ease-out" : { c1 : [0,0], c2 : [0.015,0.83], c3 : [0.375,0.995], c4 : [1,1]},
+	"ease-in-and-out" :	{ c1 : [0,0], c2 : [0.995,-0.015], c3 : [0.025,1.02], c4 : [1,1]},
+	"linear" : { c1 : [0,0], c2 : [1,1]},
+	"inverse-in-and-out" : { c1 : [0,0], c2 : [-0.045,0.72], c3 : [1.09,0.22], c4 : [1,1]},
+	"unhook" : { c1 : [0,0], c2 : [0.19,-0.72], c3 : [-0.05,1.085], c4 : [1,1]},
+	"overshoot" : { c1 : [0,0], c2 : [0.87,-0.095], c3 : [0.8,1.55], c4 : [1,1]},
+	"pop" : { c1 : [0,0], c2 : [0.595,-0.795], c3 : [0.46,1.6], c4 : [1,1]}
 };
 
 var Ease = function(){
@@ -22,7 +22,13 @@ Ease.prototype = {
 
 		if(presets[preset]){
 
-			self.curve = presets[preset];
+			self.curve = Bezier(presets[preset]);
+
+			if(preset==="linear"){
+
+				self.curve.isLinear();
+
+			}
 
 			return function( time ){
 
